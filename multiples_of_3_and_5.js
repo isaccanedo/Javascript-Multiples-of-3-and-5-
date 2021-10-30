88cc9377d74b39fb3ed76ec2d0fc05a9b6eb0632
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2021-10-30 18:55:36 -0300

    Update multiples_of_3_and_5.js

--- multiples_of_3_and_5.js
+++ multiples_of_3_and_5.js
@@ -4,7 +4,6 @@
 Problem Description: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
 The sum of these multiples is 23.
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
-Courtesy of ProjectEuler.net
 */
 
 function solution(number){
